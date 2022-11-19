1a5727966ac361e09da0a0dc7391b66390599879
Richard Beneyton <richard.beneyton@gmail.com>
2022-11-19 12:54:38 +0100

    add itertools + basic read code

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,3 +1,16 @@
+use itertools::Itertools;
+
+pub fn get_stdin_line() -> String {
+    use std::io;
+
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+    line.trim_end().to_string()
+}
+
 fn main() {
+    let mut lines = Vec::new();
+    lines.push(get_stdin_line());
+
     unimplemented!();
 }
