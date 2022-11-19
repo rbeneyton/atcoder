46bce3070e9ac8ce726d8349af596ae3ba86db5b
Richard Beneyton <richard.beneyton@gmail.com>
2022-11-19 13:09:48 +0100

    a OK

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -9,8 +9,25 @@ pub fn get_stdin_line() -> String {
 }
 
 fn main() {
-    let mut lines = Vec::new();
-    lines.push(get_stdin_line());
+    let (N, K) = get_stdin_line().split(' ')
+        .map(|x| x.to_string())
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect_tuple()
+        .unwrap();
+    let mut A = get_stdin_line().split(' ')
+        .map(|x| x.to_string())
+        .map(|x| x.parse::<u16>().unwrap())
+        .collect::<Vec<_>>();
+    assert_eq!(A.len(), N);
+    let mut start = 0;
 
-    unimplemented!();
+    for _ in 0..K {
+        start += 1;
+        A.push(0); // TODO circular buffer
+    }
+
+    for i in start..(A.len()) {
+        print!("{} ", A[i]);
+    }
+    println!("");
 }
